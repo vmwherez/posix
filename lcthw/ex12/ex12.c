9ae467f7a145a1a0c87eede5755b81fd33d7ca6c
vmwherez <m.vincent83@gmail.com>
2023-12-30 10:13:25 -0700

    sizeof in bytes

--- lcthw/ex12/ex12.c
+++ lcthw/ex12/ex12.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 	int areas[] = { 10,12,13,14,20 };
 	char name[] = "Zed";
 
-	char full_name = { 
+	char full_name[] = { 
 	'Z', 'e', 'd',
 	' ', 'A', '.',
 	'S', 'h', 'a', 'w', '\0' // null byte
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	printf("the size of name: (char[]): %ld\n", sizeof(name));
 	printf("The number of chars: %ld\n ", sizeof(name) / sizeof(char));
 
-	printf("The size of full_name (char[]):  ", sizeof(char));
+	printf("The size of full_name (char[]):  ", sizeof(full_name));
 	printf("The number of chars: %ld\n", sizeof(full_name) / sizeof(char));
 
 	printf("name=\"%s\" and full_name=\"%s\"\n", name, full_name);
